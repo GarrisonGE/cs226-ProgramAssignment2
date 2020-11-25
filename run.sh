fc374def1c85657b8a5905e5148ea09a333efbc8
GarrisonGE <605265195@qq.com>
2020-11-24 21:28:57 -0800

    update script

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 mvn clean compile package
-#execute the script: ./run.sh <file path of test file>
+
+# hard code file path, execute the script: ./run.sh
+#spark-submit --master local target/spark-scala-1.0-SNAPSHOT.jar ../nasa.tsv
+
+# run with parameter, execute the script: ./run.sh <file path of test file>
 spark-submit --master local target/spark-scala-1.0-SNAPSHOT.jar $1
+
+
